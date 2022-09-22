c9e7355b7db60d2e8400672177bea9cf2f7e4ef6
Arshad Siddiqui <ferozarshad123@gmail.com>
2022-09-22 16:17:45 +0100

    Overhaul of TimeError class

--- lib/time_error.rb
+++ lib/time_error.rb
@@ -5,29 +5,22 @@ require 'net/http'
 class TimeError
   # Returns difference in seconds between server time
   # and the time on this computer
-  def initialize(api)
-    @api = api
+  def initialize(requester)
+    @requester = requester
   end
-
-  def error
-    return (get_server_time - Time.now + 0.52).abs.round
+  def error(current_time)
+    return get_server_time - current_time
   end
-
-  # There seems to be some unreproducable delay of around 0.52s
-  # which does lead to bugs.
-  # What is terrible is that this number changes frequently.
-  # It started off at 0.26 and assumably gets worse each test
-  # i add.
 
   private
 
   def get_server_time
-    text_response = @api.get(URI("https://worldtimeapi.org/api/ip"))
+    text_response = @requester.get(URI("https://worldtimeapi.org/api/ip"))
     json = JSON.parse(text_response)
+    p "Time from 'server': #{DateTime.parse(json["utc_datetime"]).to_time}"
     return DateTime.parse(json["utc_datetime"]).to_time
   end
 end
 
-# Net::HTTP is what is being replaced by a double
-# e.g.
 # time_error = TimeError.new(Net::HTTP)
+# p time_error.error(Time.now)
--- spec/time_error_spec.rb
+++ spec/time_error_spec.rb
@@ -1,64 +1,23 @@
 require 'time_error'
 
 RSpec.describe TimeError do
-  describe '#error' do
-    context 'Delay simulated to 10s' do
-      it 'returns the 10s time delay' do
-        api = double :fake_api
-        delay_10s = DateTime.now + Rational(10, 86400)
-        json = {
-          'utc_datetime' => delay_10s
-        }
-        json_string = JSON[json]
-        allow(api).to receive(:get).with(URI("https://worldtimeapi.org/api/ip"))
-        .and_return(json_string)
-        time_error = TimeError.new(api)
-        expect(time_error.error).to eq 10
-      end
-    end
+    describe "#error" do
+        it "returns the difference in seconds between the remote time and time on the computer" do
+            fake_requester = double :fake_requester
+            allow(fake_requester).to receive(:get).with(URI("https://worldtimeapi.org/api/ip")).and_return"{\"abbreviation\":\"BST\",\"client_ip\":\"2a02:c7e:3696:f800:2cbe:b6d:c951:db08\",\"datetime\":\"2022-09-22T14:55:46.710722+01:00\",\"day_of_week\":4,\"day_of_year\":265,\"dst\":true,\"dst_from\":\"2022-03-27T01:00:00+00:00\",\"dst_offset\":3600,\"dst_until\":\"2022-10-30T01:00:00+00:00\",\"raw_offset\":0,\"timezone\":\"Europe/London\",\"unixtime\":1663854946,\"utc_datetime\":\"2022-09-22T14:59:10+00:00\",\"utc_offset\":\"+01:00\",\"week_number\":38}" 
+            time = Time.new(2022, 9, 22, 14, 59, 10, 00)
+            p "time being tested: #{time}"
+            time_error = TimeError.new(fake_requester)
+            expect(time_error.error(time)).to eq 0
+        end
 
-    context 'No time delay' do
-      it 'returns 0' do
-        api = double :fake_api
-        json = {
-          'utc_datetime' => DateTime.now
-        }
-        json_string = JSON[json]
-        allow(api).to receive(:get).with(URI("https://worldtimeapi.org/api/ip"))
-        .and_return(json_string)
-        time_error = TimeError.new(api)
-        expect(time_error.error).to eq 0
-      end
+        it "10 seconds difference" do
+          fake_requester = double :fake_requester
+          allow(fake_requester).to receive(:get).with(URI("https://worldtimeapi.org/api/ip")).and_return"{\"abbreviation\":\"BST\",\"client_ip\":\"2a02:c7e:3696:f800:2cbe:b6d:c951:db08\",\"datetime\":\"2022-09-22T14:55:46.710722+01:00\",\"day_of_week\":4,\"day_of_year\":265,\"dst\":true,\"dst_from\":\"2022-03-27T01:00:00+00:00\",\"dst_offset\":3600,\"dst_until\":\"2022-10-30T01:00:00+00:00\",\"raw_offset\":0,\"timezone\":\"Europe/London\",\"unixtime\":1663854946,\"utc_datetime\":\"2022-09-22T14:59:10+00:00\",\"utc_offset\":\"+01:00\",\"week_number\":38}" 
+          time = Time.new(2022, 9, 22, 14, 59, 00, 00)
+          p "time being tested: #{time}"
+          time_error = TimeError.new(fake_requester)
+          expect(time_error.error(time)).to eq 10
+        end
     end
-
-    context '5s delay' do
-      it 'returns 5' do
-        api = double :fake_api
-        delay_5s = DateTime.now + Rational(5, 86400)
-        json = {
-          'utc_datetime' => delay_5s
-        }
-        json_string = JSON[json]
-        allow(api).to receive(:get).with(URI("https://worldtimeapi.org/api/ip"))
-        .and_return(json_string)
-        time_error = TimeError.new(api)
-        expect(time_error.error).to eq 5
-      end
-    end
-
-    context 'Delay 100s' do
-      it 'returns 100' do
-        api = double :fake_api
-        delay_100s = DateTime.now + Rational(100, 86400)
-        json = {
-          'utc_datetime' => delay_100s
-        }
-        json_string = JSON[json]
-        allow(api).to receive(:get).with(URI("https://worldtimeapi.org/api/ip"))
-        .and_return(json_string)
-        time_error = TimeError.new(api)
-        expect(time_error.error).to eq 100
-      end
-    end
-  end
 end
